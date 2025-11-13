5e94b96a13f1097c26d3644ffb3e03d245182e9a
Vanessa Martin <39349678+vsm1996@users.noreply.github.com>
2025-11-13 23:09:53 +0000

    change widths of containers

--- component/card/styles.ts
+++ component/card/styles.ts
@@ -2,10 +2,10 @@ export const cardContainerStyles =
   "flex flex-col md:flex-row gap-(--space-300) bg-(--white) md:rounded-(--space-400) drop-shadown-(--shadow)";
 
 export const resultContainerStyles =
-  "md:min-w-[338px] text-center bg-linear-(--gradient-dark) py-(--space-400) px-[57.5px] md:py-[45px] md:px-[69px] rounded-b-(--space-400) md:rounded-(--space-400) flex flex-col items-center gap-(--space-300)";
+  "w-full md:w-[338px] text-center bg-linear-(--gradient-dark) py-(--space-400) px-[57.5px] md:py-[45px] md:px-[69px] rounded-b-(--space-400) md:rounded-(--space-400) flex flex-col items-center gap-(--space-300)";
 
 export const summaryContainerStyles =
-  "w-full h-full px-[30px] pb-[30px] md:px-0 md:pr-[40] md:py-[44.5px] flex flex-col gap-(--space-300)";
+  "md:w-[348px] h-full px-[30px] pb-[30px] md:px-0 md:pr-[40] md:py-[44.5px] flex flex-col gap-(--space-300)";
 
 export const buttonStyles =
   "z-0 overflow-hidden relative flex flex-col justify-center items-center cursor-pointer px-[120px] py-[16.5px] rounded-full bg-(--navy-950) text-(--white) text-5-bold group";
